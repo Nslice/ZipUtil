3ba3fa577137e4fe53e2b6bb1e2333fcacfa8ae9
Talgat <tairovtalgat@gmail.com>
2018-11-08 21:06:42 +0600

    Пример cоздания исполняемого jar

---
+++ jar/makejar.cmd
@@ -0,0 +1,6 @@
+REM Создание исполняемого jar-архива
+REM в винде можно просто как экзешник запускать
+
+javac JarExample.java
+jar cvfe JarExample.jar JarExample JarExample.class
+java -jar JarExample.jar
